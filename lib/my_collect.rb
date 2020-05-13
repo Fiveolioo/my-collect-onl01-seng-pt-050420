5578a6262b77dbea5e048d20326406dbd252315d
Richard Cho <96richardohc@gmail.com>
2020-05-13 00:34:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
 collection = []
     counter = 0
       my_collect(collection)
-      collection << yield (collections)
+      collection << yield (languages)
         counter += 1
       end
 end
